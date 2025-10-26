938b8d7b15e8ede09ef928d54eda41d11b8be8db
sangyeon kim <greyhairchooselife@gmail.com>
2025-10-27 06:29:36 +0900

    feat: implicit 기본구현

--- mm.c
+++ mm.c
@@ -42,9 +42,120 @@ team_t team = {
 #define NEXT_BP(ptr) ((char*)(ptr) + GET_SIZE(((char*)(bp) - WSIZE)))
 #define PREV_BP(ptr) ((char*)(ptr) - GET_SIZE(((char*)(bp) - DSIZE)))
 
-int mm_init(void) { return 0; }
+static char* heap_listp;  // heap area starting point
 
-void* mm_malloc(size_t size) { return NULL; }
+static void* coalesce(void* bp) {
+    size_t prev_alloc = GET_ALLOC(FOOTER_P(PREV_BP(bp)));
+    size_t next_alloc = GET_ALLOC(HEADER_P(NEXT_BP(bp)));
+    size_t size = GET_SIZE(HEADER_P(bp));
+
+    if (prev_alloc && next_alloc) {
+        return bp;
+    } else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HEADER_P(NEXT_BP(bp)));
+        PUT_COMBI(HEADER_P(bp), COMBINE(size, 0));
+        PUT_COMBI(FOOTER_P(bp), COMBINE(size, 0));
+    } else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HEADER_P(PREV_BP(bp)));
+        PUT_COMBI(FOOTER_P(bp), COMBINE(size, 0));  // no need to move
+        PUT_COMBI(HEADER_P(PREV_BP(bp)),
+                  COMBINE(size, 0));  // to the prev block
+        bp = PREV_BP(bp);
+
+    } else {
+        size +=
+            GET_SIZE(HEADER_P(PREV_BP(bp))) + GET_SIZE(FOOTER_P(NEXT_BP(bp)));
+        PUT_COMBI(HEADER_P(PREV_BP(bp)), COMBINE(size, 0));
+        PUT_COMBI(FOOTER_P(NEXT_BP(bp)), COMBINE(size, 0));
+        bp = PREV_BP(bp);
+    }
+
+    return bp;
+}
+
+// extend heap size
+// Returns NULL if failed, or Block Pointer that might be coalesced.
+static void* extend_heap(size_t words) {
+    char* bp;
+    size_t size;
+
+    // align
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+
+    // set break and get block pointer
+    if ((long)(bp = mem_sbrk(size)) == -1) return NULL;
+
+    PUT_COMBI(HEADER_P(bp), COMBINE(size, 0));
+    PUT_COMBI(FOOTER_P(bp), COMBINE(size, 0));
+    PUT_COMBI(HEADER_P(NEXT_BP(bp)), COMBINE(0, 1));  // epilogue
+    return coalesce(bp);
+}
+
+int mm_init(void) {
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void*)-1) return -1;
+
+    PUT_COMBI(heap_listp, 0);
+    PUT_COMBI(heap_listp + (1 * WSIZE), COMBINE(DSIZE, 1));
+    PUT_COMBI(heap_listp + (2 * WSIZE), COMBINE(DSIZE, 1));
+    PUT_COMBI(heap_listp + (3 * WSIZE), COMBINE(0, 1));
+    heap_listp += DSIZE;
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL) return -1;
+
+    return 0;
+}
+
+// First-fit search
+static void* find_fit(size_t asize) {
+    void* bp;
+
+    for (bp = heap_listp; GET_SIZE(HEADER_P(bp)) > 0; bp = NEXT_BP(bp))
+        if ((!GET_ALLOC(HEADER_P(bp))) && (asize <= GET_SIZE(HEADER_P(bp))))
+            return bp;
+
+    return NULL;
+}
+
+static void place(void* bp, size_t asize) {
+    size_t csize = GET_SIZE(HEADER_P(bp));
+
+    // at least one minimal block, split it
+    if ((csize - asize) >= (2 * DSIZE)) {
+        PUT_COMBI(HEADER_P(bp), COMBINE(asize, 1));
+        PUT_COMBI(FOOTER_P(bp), COMBINE(asize, 1));
+        bp = NEXT_BP(bp);
+        PUT_COMBI(HEADER_P(bp), COMBINE(csize - asize, 1));
+        PUT_COMBI(FOOTER_P(bp), COMBINE(csize - asize, 1));
+    } else {
+        PUT_COMBI(HEADER_P(bp), COMBINE(csize, 1));
+        PUT_COMBI(FOOTER_P(bp), COMBINE(csize, 1));
+    }
+}
+
+void* mm_malloc(size_t size) {
+    size_t asize;       // adjusted block size
+    size_t extendSize;  // if no fit, extend this size
+    char* bp;
+
+    if (!size) return NULL;
+
+    if (size <= DSIZE)
+        asize = 2 * DSIZE;
+    else
+        asize = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
+
+    if ((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
+    }
+
+    // no fitted block found.
+    extendSize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendSize / WSIZE)) == NULL) return NULL;
+
+    place(bp, asize);
+    return bp;
+}
 
 void mm_free(void* ptr) {}
 
